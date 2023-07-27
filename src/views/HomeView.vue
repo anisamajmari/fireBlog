2d839dcf7c521b0c23cda0c4a5c56ad08782f7f2
Anisa Majmari <anisamajmari@gmail.com>
2023-07-27 15:21:27 +0200

    Blog post component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <blog-post :post="welcomeScreen"></blog-post>
+    <blog-post :post="post" v-for="(post, index) in sampleBlogPost" :key="index"></blog-post>
+  </div>
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost.vue';
 export default {
-  components: {}
+  components: { BlogPost },
+  data() {
+    return {
+      welcomeScreen: {
+        title: 'Welcome!',
+        blogPost:
+          'Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!',
+        welcomeScreen: true,
+        photo: 'coding'
+      },
+      sampleBlogPost: [
+        {
+          title: 'This is the title',
+          blogHTML: 'This is a filler blog post title!',
+          blogCoverPhoto: 'beautiful-stories'
+        },
+        {
+          title: 'This is the title',
+          blogHTML: 'This is a filler blog post title!',
+          blogCoverPhoto: 'designed-for-everyone'
+        }
+      ]
+    };
+  }
 };
 </script>
